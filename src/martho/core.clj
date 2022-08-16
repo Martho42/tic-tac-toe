772cb484a92ffc7f6b74eef41a5e9ef79f7a2bc6
Paul Stadig <paul.stadig@sovasage.com>
2022-08-16 15:06:34 -0400

    Refactor board to include state as submap

--- src/martho/core.clj
+++ src/martho/core.clj
@@ -3,22 +3,21 @@
             [clojure.string :as str]))
 
 (def board-indices
-  (mapcat #((fn [v c] 
+  (mapcat #((fn [v c]
               (map (fn [x] (vector v x)) c))
-            % (range 3)) 
+            % (range 3))
           (range 3)))
 
 (identity board-indices)
 
-(def start-board 
-  (assoc
-    (apply merge (map #(hash-map % 0) board-indices))
-    :turn 1))
+(def start-board
+  {:turn 1
+   :state (apply merge (map #(hash-map % 0) board-indices))})
 
 (identity start-board)
 
 (defn get-marker [p]
-  (cond 
+  (cond
     (= 0 p) "-"
     (= 1 p) "X"
     (= -1 p) "O"))
@@ -34,9 +33,9 @@
   board)
 
 (defn mark [board square]
-  (if (= (board square) 0)
+  (if (= (get-in board [:state square]) 0)
     (-> board
-        (assoc  square (:turn board))
+        (assoc-in [:state square] (:turn board))
         (update :turn * -1))
     (do (prn "Invalid Square")
         board)))
@@ -44,12 +43,8 @@
 (defn get-squares-keys [board]
   (filter vector? (keys start-board)))
 
-
-(defn get-squares [board]
-  (filter #((key %) vector?) (keys start-board)))
-
 (def win-sequences
-  (let [squares (get-squares-keys start-board)
+  (let [squares (keys (:state start-board))
         hseqs (map (fn [x] (filter #(= x (first %)) squares)) (range 3))
         vseqs (map (fn [x] (filter #(= x (second %)) squares)) (range 3))
         cross1 [(filter #(= (first %) (second %)) squares)]
@@ -60,21 +55,21 @@
   (let [victor (first (filter #(= 3 (abs %))
                               (map #(apply + %)
                                    (map (fn [x]
-                                          (map #(board %) x))
+                                          (map #(get-in board [:state %]) x))
                                         win-sequences))))
 
         tie? (= 8 (count (filter #(and (some #{1} %) (some #{-1} %))
-                              (map (fn [x]
-                                          (map #(board %) x))
-                                        win-sequences))))]
+                                 (map (fn [x]
+                                        (map #(get-in board [:state %]) x))
+                                      win-sequences))))]
     (cond
-      victor (do 
+      victor (do
                (prn (str "Player " (get-marker (/ victor 3)) " wins!"))
                (assoc board :turn 0))
       tie? (do (prn "It's a tie!")
                (assoc board :turn 0))
       true (do (prn (str "Player " (get-marker (:turn board)) "'s turn. Enter a letter followed by a number:"))
-              board))))
+               board))))
 
 (defn turn [board square]
   (-> board
@@ -86,12 +81,9 @@
   (print-board start-board)
   (check-end start-board)
   (loop [board start-board]
-    (cond 
-      (= 1 (:turn board)) (turn board (filter #( get-squares board)))
+    (cond
+      (= 1 (:turn board))
+      (turn board (filter #(zero? (val %)) (:state board)))
       (= -1 (:turn board)) (let [line (read-line)]
-                             (recur (turn board [(- (int (first line)) 97) 
-                                                 (Character/digit (last line) 10)])))
-      :else
-
-      #_(not= 0 (:turn board))
-      )))
+                             (recur (turn board [(- (int (first line)) 97)
+                                                 (Character/digit (last line) 10)]))))))
